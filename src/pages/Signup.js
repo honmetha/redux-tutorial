5ebab411d98793a25b829c2aa4985e164cc545e0
Hon <dennis.mejen@gmail.com>
2019-12-20 00:50:27 +0700

    up to Sign

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
+import { Row, Col, Input, Button } from 'antd'
 
 export default class Signup extends Component {
   render() {
     return (
-      <div>
-        Signup
-      </div>
+      <Row>
+        <Col span={2}></Col>
+        <Col span={10}>
+          <img src="LoginPic.jpg" alt="Humans" width="100%" />
+        </Col>
+        <Col span={10}>
+          Username:
+          <Input placeholder="Username" />
+          Password:
+          <Input placeholder="Password" />
+        <Row>
+          <Col span={3}>
+            <Button type="primary">Login</Button>
+          </Col>
+          <Col span={3}>
+            <Button type="danger">Signup</Button>
+          </Col>
+        </Row>
+        </Col>
+        <Col span={2}></Col>
+      </Row>
     )
   }
 }
