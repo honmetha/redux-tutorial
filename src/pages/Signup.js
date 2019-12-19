f197eab13a22dad12cace6ea305dfa8704712622
Hon <dennis.mejen@gmail.com>
2019-12-20 01:18:52 +0700

    Login Responsive

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -4,7 +4,7 @@ import { Row, Col, Input, Button } from 'antd'
 export default class Signup extends Component {
   render() {
     return (
-      <Row>
+      <Row type="flex" justify="center" align="middle" style={{ height: "100%" }}>
         <Col span={2}></Col>
         <Col span={10}>
           <img src="LoginPic.jpg" alt="Humans" width="100%" />
@@ -15,11 +15,15 @@ export default class Signup extends Component {
           Password:
           <Input placeholder="Password" />
         <Row>
-          <Col span={3}>
-            <Button type="primary">Login</Button>
+          <Col span={12}>
+            <Row type="flex" justify="end" style={{ marginRight: "2.5px", marginTop: "5px" }}>
+              <Button type="primary">Login</Button>
+            </Row>
           </Col>
-          <Col span={3}>
-            <Button type="danger">Signup</Button>
+          <Col span={12}>
+            <Row type="flex" justify="start" style={{ marginLeft: "2.5px", marginTop: "5px" }}>
+              <Button type="danger">Signup</Button>
+            </Row>
           </Col>
         </Row>
         </Col>
