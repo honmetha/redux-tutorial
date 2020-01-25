f0b283a1e1fe5ba14718548391f285cc81f0b406
Hon <dennis.mejen@gmail.com>
2020-01-26 04:36:23 +0700

    Prepare to test redux

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
 import React from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import { Route, Switch } from 'react-router-dom'
-import { Redirect } from 'react-router'
-import { Layout } from 'antd';
-
-const { Header, Footer, Content } = Layout;
 
 function App() {
   return (
-    <Layout>
-      <Header>Header</Header>
-      <Content>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={Signup} />
-          <Redirect to="/" />
-        </Switch>
-      </Content>
-      <Footer>Footer</Footer>
-    </Layout>
+    <div>
+      Hello World
+    </div>
   );
 }
 
