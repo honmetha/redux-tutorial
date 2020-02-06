b1aad5934e698806c63158665c3a02d195fa225f
Hon <dennis.mejen@gmail.com>
2020-02-07 00:51:11 +0900

    Add connect to ButtonUp

--- src/components/ButtonUp.js
+++ src/components/ButtonUp.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addCounter } from '../redux/actions/actions'
 
-export default class ButtonUp extends Component {
+class ButtonUp extends Component {
   render() {
     return (
-      <button>Plus</button>
+      <button onClick={this.props.HandleAdd}>Plus</button>
     )
   }
 }
+
+const mapDispatchToProps = {
+  HandleAdd: addCounter
+}
+
+export default connect(null, mapDispatchToProps)(ButtonUp)
