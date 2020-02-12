b261013d1001b73f7e02afa032e3bda9a0bbbd65
Hon <dennis.mejen@gmail.com>
2020-02-12 16:25:39 +0700

    Add InputBox

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addNumberToCounter, multiplyNumber } from '../redux/actions/actions'
 
-export default class InputBox extends Component {
+class InputBox extends Component {
+  state = {
+    onNumber: 0,
+    multiplyNumber: 0
+  }
+
+handleOnChange = (value, fieldName) => {
+  this.setState({ [fieldName] : value });
+}
+
   render() {
     return (
       <div>
-        
+        <input onChange={(e) => this.handleOnChange(e.target.value, "onNumber")} />
+        <button onClick={() => this.props.addNumber(this.state.onNumber)}>Add</button>
+        <br/>
+        <input onChange={(e) => this.handleOnChange(e.target.value, "multiplyNumber")} />
+        <button onClick={() => this.props.multiplyNum(this.state.multiplyNumber)}>Multiply</button>
+        <button>Divine</button>
       </div>
-    )
+    );
   }
 }
+
+const mapDispatchToProps = {
+  addNumber : addNumberToCounter,
+  multiplynum : multiplyNumber
+}
+
+export default connect(null, mapDispatchToProps)(InputBox);
