24b72b96e1958b1f2f10bf8e19e9d01f7517fe01
Hon <dennis.mejen@gmail.com>
2020-02-16 23:30:17 +0700

    Add app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
+import { connect } from 'react';
+import CounterDisplay from './components/ButtonDown';
+import ButtonUp from './components/ButtonUp';
+import ButtonDown from './components/ButtonDown';
+import ButtonVisibility from './components/ButtonVisibility';
+import InputBox from './components/InputBox';
 
-function App() {
-  return (
-    <div>
-      Hello World
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {this.props.isShow == true ? (
+          <>
+            <CounterDisplay />
+            <ButtonUp />
+            <ButtonDown />
+          </>
+        ) : null}
+        <br />
+        <ButtonVisibility />
+        <br />
+        <InputBox />
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isShow: state.visible
+  };
+};
+
+export default connect(mapStateToProps, null)(App);
